b7756c3b905ea583330736851fcffd4c1db31bc6
Nicole Candiotti <nccandiotti@gmail.com>
2022-02-23 10:47:10 -0500

    commit tree 55449e50e8e38555dda1981003a9ba82ac4c42e0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arrayOfNames)
+    arrayOfNames.map{|name| badge_maker(name)}
+end
+
+def assign_rooms(arrayOfNames)
+    arrayOfNames.map.with_index(1) do |name, index| 
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(attendees).each do |assignment|
+      puts assignment
+    end
+  end
